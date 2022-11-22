a66eb0127b42e7c211d5b129e92df6dfcfda982f
William <322969@tldsb.me>
2022-11-22 18:37:48 -0500

    nov 22 auto stuff

--- src/main/java/frc/robot/commands/AutoTest_03.java
+++ src/main/java/frc/robot/commands/AutoTest_03.java
@@ -7,39 +7,61 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveBase;
-
-public class AutoTest_02 extends CommandBase {
-
+  
+public class AutoTest_03 extends CommandBase {
   private final DriveBase driveBase;
   private int counter = 0;
   private int target;
-  private int firstForwardMove;
-  private int firstStop;
-  private int firstBackwardsMove;
-  private double distance; 
-  private double throttleValue_01;
-  private double aiden;
-  /** Creates a new AutoTest_02. */
-  public AutoTest_02(DriveBase driveBase) {
-    this.driveBase = driveBase;
-    target = (int)(3*50);
+  private int angle;
+  private double rotationValue1;
+  private int[] distances = {0, 0, 0, 0, 0, 0};
+  private double[] distanceValues = {0, 0, 0, 0, 0, 0};
+  private int[] checkpoints = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    //1668x^4.7	-- First Test formula, just need to reverse it to go from distance to speed
+
+  public AutoTest_03(DriveBase driveBase) {
+    this.driveBase = driveBase;
+    target = (int) (23*50);
+/*
+    Angle formula
+    angle = 45;
+    david = angle/10635;
+    rotationValue01 = Math.pow(david, (1/6.05));
+
+    Angle Code here
+*/
+    angle = 90;
+    rotationValue1 = Math.pow((angle/10635), (1/6.05));
+
+/*
+    Distance Formula
     distance = 60;
     aiden = distance/1668;
     throttleValue_01 = Math.pow(aiden, (1/4.7));
-    
+
+    Distance Code here
+*/
+    distances[0] = 81;
+    distances[1] = 27;
+    distances[2] = 32;
+    distances[3] = 22;
+    distances[4] = 49;
+    distances[5] = 17;
+
+    for (int i = 0; i <= distanceValues.length; i++){
+    distanceValues[i] = Math.pow(distances[i]/1668, (1/4.7));
+    }
+
     addRequirements(driveBase);
-    // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    firstForwardMove = 50;
-    firstStop = 50 + firstForwardMove;
-    firstBackwardsMove = 50 + firstStop;
-
+    checkpoints[0] = 50;
+    for (int i = 1; i <= checkpoints.length; i++){
+      checkpoints[i] = 50 + checkpoints[i-1];
+    }
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -48,15 +70,10 @@ public class AutoTest_02 extends CommandBase {
     if(counter == 0){
       driveBase.arcadeDrive(0.0, 0.0);
     }
-    if(counter > 0 && counter < firstForwardMove){
-      driveBase.arcadeDrive(-throttleValue_01, 0.0);
+    if(counter > 0 && counter < checkpoints[0]){
+      driveBase.arcadeDrive(rotationValue1, 0.0);
     }
-    if(counter >= firstForwardMove && counter < firstStop){
-      driveBase.arcadeDrive(0.0, 0.0);
-    }
-    if(counter >= firstStop && counter < firstBackwardsMove){
-      driveBase.arcadeDrive(throttleValue_01, 0.0);
-    }
+
     counter++;
     SmartDashboard.putNumber("COUNTER", counter);
   }
